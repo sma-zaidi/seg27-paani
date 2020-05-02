ea8fef45c492a97f2447745f6a326b5ab1c7626e
Abbas <21100166@lums.edu.pk>
2020-05-02 20:33:42 +0500

    added comments

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -5,20 +5,7 @@ const Company = require('../models/Company')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('users');
-})
-
-router.get('/home', (req, res, next) => {
-    if (req.session.loggedin) { 
-        return res.send('HOME');
-    }
-    else {
-        res.send('no')
-        req.session.loggedin = true;
-    }
-})
-
+// user registration
 router.post('/register', async (req, res, next) => {
 
     var {email, password, account_type} = req.body;
@@ -27,12 +14,12 @@ router.post('/register', async (req, res, next) => {
         return res.json({error: 'Atleast one of the required fields: email, or password is missing.'});
     }
 
-    if (account_type == undefined) account_type = 'CUSTOMER';
+    if (account_type == undefined) account_type = 'CUSTOMER'; // account_type defaults to 'CUSTOMER' if not explicitly provided
     if (account_type != 'CUSTOMER' && account_type != 'COMPANY') {
         return res.json({error: 'Incorrect value in account_type field: should be either \'CUSTOMER\' or \'COMPANY\''});
     }
 
-    try {
+    try { 
         if(await User.exists(email) == true) {
             return res.json({error: 'The provided email address already has an account associated with it.'});
         }
@@ -40,7 +27,8 @@ router.post('/register', async (req, res, next) => {
         return res.json({error: error});
     }
 
-    if (account_type == 'CUSTOMER') { // registering a customer account
+    // handle customer registration
+    if (account_type == 'CUSTOMER') {
 
         var {name, contact_number, address, location} = req.body;
 
@@ -63,7 +51,9 @@ router.post('/register', async (req, res, next) => {
 
         res.json({error: false, msg: 'Record inserted, redirect to login.'});
     }
-    else if (account_type == 'COMPANY') { // registering a company account
+
+    // handle company registration
+    else if (account_type == 'COMPANY') {
 
         var {name, ntn, contact_number, address, location} = req.body;
 
@@ -80,7 +70,7 @@ router.post('/register', async (req, res, next) => {
         try {
             await Company.create(20, name, ntn, contact_number, address, location);
         } catch (error) {
-            User.destroy(userid);
+            User.destroy(userid); // user is created before customer, so if something goes wrong while creating the customer, need to roll back.
             return res.json({error: error});
         }
 
@@ -89,6 +79,7 @@ router.post('/register', async (req, res, next) => {
 
 })
 
+// user login
 router.post('/login', async (req, res, next) => {
 
     var {email, password} = req.body;
@@ -106,13 +97,14 @@ router.post('/login', async (req, res, next) => {
     }
 
     try {
-        user = await User.getByEmail(email);
+        user = await User.getByEmail(email); // get the actual credentials associated with this email to validate password
     } catch (error) {
         return res.json({error: error});
     }
 
     if (password != user.password) return res.json({error: 'Invalid login credentials'});
 
+    // handle customer login
     if (user.account_type == 'CUSTOMER') {
         try {
             result = await Customer.getById(user.id);
@@ -122,6 +114,7 @@ router.post('/login', async (req, res, next) => {
         }
     }
 
+    // handle company login
     else if (user.account_type == 'COMPANY') {
         try {
             result = await Company.getById(user.id);
