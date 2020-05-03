fd98d0cc915fa83866df7e629c9f692531517b05
ibrahim <ibra_noor@hotmail.com>
2020-05-04 01:32:58 +0500

    incorporated hamza's work

--- front-end/lib/screens/customersideapp/customer_home_screen.dart
+++ front-end/lib/screens/customersideapp/customer_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:paani/Screens/drawer.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -9,12 +10,14 @@ import 'package:paani/screens/customersideapp/customerorderplacement.dart';
 //import './Screens/drawer.dart';
 class CustomerHomeScreen extends StatefulWidget {
   @override
-  _HomeScreenState createState() => new _HomeScreenState();
+  HomeScreenState createState() => new HomeScreenState();
 }
 
-class _HomeScreenState extends State<CustomerHomeScreen> {
-  List<dynamic> data;
-  Future<String> getData() async {
+class HomeScreenState extends State<CustomerHomeScreen> {
+  bool isSearching = false;
+  List<dynamic> companies;
+  List<dynamic> searchCompanies = [];
+  Future<void> getData() async {
     //var type -- http.response
     var response = await http.get(
         "https://seg27-paani-backend.herokuapp.com/companies",
@@ -22,7 +25,22 @@ class _HomeScreenState extends State<CustomerHomeScreen> {
     //print(response.body);
     this.setState(() {
       Map<String, dynamic> map = json.decode(response.body);
-      data = map["message"];
+      companies = map["message"];
+      searchCompanies = companies;
+    });
+    //String name=data[0]["name"];
+    //List<dynamic> data= jsonDecode(response.body);
+    //print(data[1]["company_id"]);
+  }
+
+  void searchCompany(String input) {
+    setState(() {
+      searchCompanies = companies
+          .where((compamy) => compamy['name']
+              .toString()
+              .toLowerCase()
+              .contains(input.toLowerCase()))
+          .toList();
     });
   }
 
@@ -37,6 +55,53 @@ class _HomeScreenState extends State<CustomerHomeScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
+        title: !isSearching
+            ? Text(
+                "Welcome",
+                style: TextStyle(color: Colors.white),
+              )
+            : TextField(
+                onChanged: (String input) {
+                  searchCompany(input);
+                },
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search',
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    )),
+              ),
+        actions: <Widget>[
+          !isSearching
+              ? IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isSearching = true;
+                    });
+                  },
+                )
+              : IconButton(
+                  icon: Icon(
+                    Icons.clear,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      searchCompanies = companies;
+                      isSearching = false;
+                    });
+                  },
+                ),
+        ],
         backgroundColor: Colors.teal,
       ),
       drawer: new DrawerDetails(),
@@ -46,105 +111,113 @@ class _HomeScreenState extends State<CustomerHomeScreen> {
             new Container(
               height: 657,
               padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: new ListView.builder(
-                  //vertical by default
-                  itemCount: data == null ? 0 : data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return new Card(
-                      elevation: 5,
-                      child: new Row(
-                        children: <Widget>[
-                          //image inside container
-                          new Container(
-                            height: 225,
-                            width: 150,
-                            decoration: new BoxDecoration(
-                              borderRadius: new BorderRadius.only(
-                                bottomLeft: new Radius.circular(5),
-                                topLeft: new Radius.circular(5),
+              child: searchCompanies.length > 0
+                  ? new ListView.builder(
+                      //vertical by default
+                      itemCount: searchCompanies.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return new Card(
+                          elevation: 5,
+                          child: new Row(
+                            children: <Widget>[
+                              //image inside container
+                              new Container(
+                                height: 225,
+                                width: 150,
+                                decoration: new BoxDecoration(
+                                  borderRadius: new BorderRadius.only(
+                                    bottomLeft: new Radius.circular(5),
+                                    topLeft: new Radius.circular(5),
+                                  ),
+                                  image: new DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: new AssetImage(
+                                      'assets/default3.png',
+                                    ),
+                                  ),
+                                ),
                               ),
-                              image: new DecorationImage(
-                                fit: BoxFit.cover,
-                                image: new AssetImage(
-                                  'assets/default3.png',
+                              new Container(
+                                padding: const EdgeInsets.all(20),
+                                height: 225,
+                                child: new Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    new Text(
+                                      searchCompanies[index]["name"],
+                                      style: new TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.teal,
+                                      ),
+                                    ),
+                                    new SizedBox(
+                                      height: 10,
+                                    ),
+                                    new Text(
+                                      'Services:',
+                                      style: new TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    new SizedBox(
+                                      height: 10,
+                                    ),
+                                    new Text(
+                                      'Base Price:',
+                                      style: new TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    new SizedBox(
+                                      height: 10,
+                                    ),
+                                    new Text(
+                                      'Price/km:',
+                                      style: new TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    new SizedBox(
+                                      height: 10,
+                                    ),
+                                    new RaisedButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    Place_Order_Screen(
+                                                      data:
+                                                          searchCompanies[index]
+                                                              ['name'],
+                                                    )));
+                                      },
+                                      child: new Text('Order'),
+                                      textColor: Colors.white,
+                                      color: Colors.teal,
+                                    ),
+                                    new SizedBox(
+                                      height: 10,
+                                    ),
+                                    new StarRating(
+                                      rating: 3.45,
+                                      starConfig: new StarConfig(
+                                        fillColor: Colors.teal,
+                                        strokeColor: Colors.teal,
+                                        size: 20,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ),
+                            ],
                           ),
-                          new Container(
-                            padding: const EdgeInsets.all(20),
-                            height: 225,
-                            child: new Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                new Text(
-                                  data[index]["name"],
-                                  style: new TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.teal,
-                                  ),
-                                ),
-                                new SizedBox(
-                                  height: 10,
-                                ),
-                                new Text(
-                                  'Services:',
-                                  style: new TextStyle(
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                new SizedBox(
-                                  height: 10,
-                                ),
-                                new Text(
-                                  'Base Price:',
-                                  style: new TextStyle(
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                new SizedBox(
-                                  height: 10,
-                                ),
-                                new Text(
-                                  'Price/km:',
-                                  style: new TextStyle(
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                new SizedBox(
-                                  height: 10,
-                                ),
-                                new RaisedButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                Place_Order_Screen()));
-                                  },
-                                  child: new Text('Order'),
-                                  textColor: Colors.white,
-                                  color: Colors.teal,
-                                ),
-                                new SizedBox(
-                                  height: 10,
-                                ),
-                                new StarRating(
-                                  rating: 3.45,
-                                  starConfig: new StarConfig(
-                                    fillColor: Colors.teal,
-                                    strokeColor: Colors.teal,
-                                    size: 20,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
+                        );
+                      })
+                  : Center(
+                      child: CircularProgressIndicator(),
+                    ),
             ),
           ],
         ),
