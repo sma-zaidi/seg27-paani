c0002ff8bd871f7ae3bde5c63e11f9510102209a
Abbas <21100166@lums.edu.pk>
2020-05-03 23:10:04 +0500

    can now get company orders by status

--- server/models/Order.js
+++ server/models/Order.js
@@ -8,6 +8,15 @@ Order = {
             return result;
         }catch (error) {throw new Error(error)}
     },
+
+    getByCompanyAndStatus: async (companyid, status) => {
+        try {
+            result = await query(`SELECT * FROM \`seg27-paani\`.orders
+                                  INNER JOIN \`seg27-paani\`.packages ON \`seg27-paani\`.orders.package_id = \`seg27-paani\`.packages.id                     
+                                  WHERE company_id = ? AND status = ?`, [companyid, status]);
+            return result;
+        } catch (error) { throw new Error(error) }
+    },
 }
 
 module.exports = Order
