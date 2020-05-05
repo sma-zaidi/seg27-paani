56dfdfdced32ea98b60b885aa850ad5a30bc7ec2
hamza-707 <55429924+hamza-707@users.noreply.github.com>
2020-05-05 17:49:54 +0500

    Added Divider

--- front-end/lib/screens/companysideapp/in_progress.dart
+++ front-end/lib/screens/companysideapp/in_progress.dart
@@ -21,93 +21,96 @@ class InProgressState extends State<InProgress> {
           ),
         ),
       ),
-      body: ListView.builder(itemBuilder: (BuildContext context, int index) {
-        return Row(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(left: 10),
-              height: 100,
-              width: 150,
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Order id:",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "Driver:",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "Address:",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "Package:",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "Deliver Date:",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              width: 15,
-            ),
-            IconButton(
-              icon: Image.asset('assets/confirmed.png'),
-              iconSize: 50,
-              onPressed: () {},
-            ),
-            SizedBox(
-              width: 15,
-            ),
-            ButtonTheme(
-              minWidth: 0,
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              child: RaisedButton(
-                onPressed: () {},
-                child: Row(
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int index) {
+          return Row(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(left: 10),
+                width: 150,
+                child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Cancel"),
                     SizedBox(
-                      width: 10,
+                      height: 10,
                     ),
-                    Icon(Icons.clear)
+                    Text(
+                      "Order id:",
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Driver:",
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Address:",
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Package:",
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Deliver Date:",
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
-                textColor: Colors.white,
-                color: Colors.teal,
               ),
-            ),
-          ],
-        );
-      }),
+              SizedBox(
+                width: 15,
+              ),
+              IconButton(
+                icon: Image.asset('assets/confirmed.png'),
+                iconSize: 50,
+                onPressed: () {},
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              ButtonTheme(
+                minWidth: 0,
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: <Widget>[
+                      Text("Cancel"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(Icons.clear)
+                    ],
+                  ),
+                  textColor: Colors.white,
+                  color: Colors.teal,
+                ),
+              ),
+            ],
+          );
+        },
+        itemCount: 10,
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+      ),
     );
   }
 }
