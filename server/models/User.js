8ca1b7f2d3c264edb2647df098b76f84f4f4ff0a
Abbas <21100166@lums.edu.pk>
2020-05-05 10:17:46 +0500

    customer reg requires contact no. company reg requires location.

--- server/models/User.js
+++ server/models/User.js
@@ -14,7 +14,8 @@ User = {
     destroy: async (userid) => { // delete by id
         try {
             result = await query(`DELETE FROM \`seg27-paani\`.users WHERE id = ?`, [userid]);
-        } catch {}
+        } catch (error) { console.log(error) } // this method is only called while handling an exception during User.create. Throwing an error
+                                               // here MIGHT cause issues.
     },
 
     exists: async (email) => { // true if a user with this email exists, false otherwise
@@ -26,7 +27,7 @@ User = {
         } catch (error) { throw new Error(error) }
     },
 
-    getByEmail: async (email) => {
+    getByEmail: async (email) => { // returns userid, email, password and account_type
         try {
 
             result = await query(`SELECT * FROM \`seg27-paani\`.users WHERE email = ?`, [email]);
