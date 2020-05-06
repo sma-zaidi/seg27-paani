320a3f673204056b01adf9e0579b736f7d2c5a32
hamza-707 <55429924+hamza-707@users.noreply.github.com>
2020-05-06 22:39:13 +0500

    Linked to DB

--- front-end/lib/screens/companysideapp/completedorders.dart
+++ front-end/lib/screens/companysideapp/completedorders.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CompletedOrders extends StatefulWidget {
   @override
@@ -8,113 +12,138 @@ class CompletedOrders extends StatefulWidget {
 }
 
 class CompletedOrdersState extends State<CompletedOrders> {
+  var completedOrders = [];
+  var loadScreen = true;
+  Future<void> getCompletedOrders() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    var responseCompleted = await http.get(
+        'https://seg27-paani-backend.herokuapp.com/orders/${pref.getString('userid')}/Complete');
+    var dataBody = jsonDecode(responseCompleted.body);
+    if (dataBody['error'] == false) {
+      setState(() {
+        completedOrders = dataBody['msg'];
+        loadScreen = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    getCompletedOrders();
+  }
+
   Widget build(BuildContext context) {
-    List<int> order = [1, 2, 3, 4];
-    return Scaffold(
-      appBar: new AppBar(
-        backgroundColor: Colors.teal,
-        centerTitle: true,
-        title: Text(
-          "Completed",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: ListView.separated(
-        itemCount: order.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Row(
-            children: <Widget>[
-              Container(
-                // width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.only(left: 10),
-                height: 100,
-                width: 180,
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return loadScreen == true
+        ? Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : Scaffold(
+            appBar: new AppBar(
+              backgroundColor: Colors.teal,
+              centerTitle: true,
+              title: Text(
+                "Completed",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            body: ListView.separated(
+              itemCount: completedOrders.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Row(
                   children: <Widget>[
+                    Container(
+                      // width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.only(left: 10),
+                      width: 180,
+                      child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Order ID: ${completedOrders[index]['orderid']}",
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Driver ID: ${completedOrders[index]['driver_id']}",
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Address: ${completedOrders[index]['delivery_address']}",
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Package ID: ${completedOrders[index]['package_id']}",
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Deliver Date: ${completedOrders[index]['delivery_time']}",
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                     SizedBox(
-                      height: 10,
+                      width: 10,
                     ),
-                    Text(
-                      "Order id: ",
-                      style: TextStyle(
-                        color: Colors.black45,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
+                    RatingBarIndicator(
+                      //TODO: implement rating
+                      itemSize: 30.0,
+                      rating: 3,
+                      // minRating: 1,
+                      direction: Axis.horizontal,
+                      itemCount: 5,
+                      itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                      itemBuilder: (context, _) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
                       ),
                     ),
-                    Text(
-                      "Driver:",
-                      style: TextStyle(
-                        color: Colors.black45,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "Address:",
-                      style: TextStyle(
-                        color: Colors.black45,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "Package:",
-                      style: TextStyle(
-                        color: Colors.black45,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "Deliver Date:",
-                      style: TextStyle(
-                        color: Colors.black45,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                    // RaisedButton(
+                    //   onPressed: () {},
+                    //   child: Row(
+                    //     children: <Widget>[
+                    //       Text("Cancel"),
+                    //       SizedBox(
+                    //         width: 10,
+                    //       ),
+                    //       Icon(Icons.clear)
+                    //     ],
+                    //   ),
+                    //   textColor: Colors.white,
+                    //   color: Colors.teal,
+                    // ),
                   ],
-                ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              RatingBarIndicator(
-                itemSize: 30.0,
-                rating: 3,
-                // minRating: 1,
-                direction: Axis.horizontal,
-                itemCount: 5,
-                itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
-                itemBuilder: (context, _) => Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                ),
-              ),
-              // RaisedButton(
-              //   onPressed: () {},
-              //   child: Row(
-              //     children: <Widget>[
-              //       Text("Cancel"),
-              //       SizedBox(
-              //         width: 10,
-              //       ),
-              //       Icon(Icons.clear)
-              //     ],
-              //   ),
-              //   textColor: Colors.white,
-              //   color: Colors.teal,
-              // ),
-            ],
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) => Divider(),
+            ),
           );
-        },
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-      ),
-    );
   }
 }
